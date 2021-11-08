b9acf8fd22df2d9183c8c36d5dd419461e03a74f
buffyhridoy <chemicalbuffy@gmail.com>
2021-11-08 21:12:48 +0600

    print face landmark

--- 1st one.py
+++ 1st one.py
@@ -6,16 +6,6 @@ import time
 from matplotlib import pyplot as plt
 import os
 
-#video capture from webcam using opencv
-cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    cv2.imshow('OpenCV Feed', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
-
 mp_holistic = mp.solutions.holistic  #holistic model
 mp_drawing = mp.solutions.drawing_utils #drawing utils
 
@@ -42,6 +32,6 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 cap.release()
 cv2.destroyAllWindows()
 
-
+print(results.face_landmarks)
 
 
