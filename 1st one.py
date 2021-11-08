ead7a02a570d093d276ad1d65c22219fcf9a7feb
buffyhridoy <chemicalbuffy@gmail.com>
2021-11-08 21:10:18 +0600

    holistic model in opencv feed

--- 1st one.py
+++ 1st one.py
@@ -16,3 +16,32 @@ while cap.isOpened():
 cap.release()
 cv2.destroyAllWindows()
 
+mp_holistic = mp.solutions.holistic  #holistic model
+mp_drawing = mp.solutions.drawing_utils #drawing utils
+
+#mediapipe detection function
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return results, image
+
+#using holistic model in opencv feed
+cap = cv2.VideoCapture(0)
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as model:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        results, image = mediapipe_detection(frame, model)
+        print(results)
+        #mp_drawing.draw_detection_results(image, results)
+        cv2.imshow('OpenCV Feed', image)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
