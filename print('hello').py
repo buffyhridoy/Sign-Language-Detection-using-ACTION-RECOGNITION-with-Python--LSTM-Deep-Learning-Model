913d5106d5a4816014cd3f148e09965b947070c8
buffyhridoy <chemicalbuffy@gmail.com>
2021-11-08 12:30:26 +0600

    importing 1st libraries

--- print('hello').py
+++ print('hello').py
@@ -1,3 +1,7 @@
-#print hello
-print('hello')
-print("checking")
+#importing dependencies
+import mediapipe as mp
+import numpy as np
+import cv2
+import time
+from matplotlib import pyplot as plt
+import os
