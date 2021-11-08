2d537afa74b0492e7cf0a09f1f5a7168705fdc95
buffyhridoy <chemicalbuffy@gmail.com>
2021-11-08 19:58:09 +0600

    opencv feed creating

--- 1st one.py
+++ 1st one.py
@@ -5,3 +5,14 @@ import cv2
 import time
 from matplotlib import pyplot as plt
 import os
+
+#video capture from webcam using opencv
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+    cv2.imshow('OpenCV Feed', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
